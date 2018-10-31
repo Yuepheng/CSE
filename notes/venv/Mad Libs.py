290f2fd10423b183cbe31d4fc789408f1edcba20
Yuepheng <42391752+Yuepheng@users.noreply.github.com>
2018-10-31 14:39:17 -0700

    Python Basic Notes

--- notes/venv/Mad Libs.py
+++ notes/venv/Mad Libs.py
@@ -3,4 +3,9 @@
 adjective1 = input("Type in an adjective")
 noun1 = input("Type in a noun")
 
-print("I saw a %s %s" % (adjective1, noun1))
+print("I was %s at a %s" % (adjective1, noun1))
+
+adjective2 = input("Type in an Adjecttive")
+noun2 = input("Type in a noun")
+
+print("Then I did %s and %s" % (adjective2, noun2 ))
