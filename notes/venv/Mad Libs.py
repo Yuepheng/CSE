be4529b6b58c84a5c9dd91870dee46be16aa4552
Yuepheng <42391752+Yuepheng@users.noreply.github.com>
2018-11-05 14:33:29 -0800

    Mad Libs as of 11/5/2018

--- notes/venv/Mad Libs.py
+++ notes/venv/Mad Libs.py
@@ -3,20 +3,51 @@
 adjective1 = input("Type in an adjective")
 noun1 = input("Type in a noun")
 
-print("I was %s at a %s" % (adjective1, noun1))
+print("I was thinking about %s at a %s again" % (adjective1, noun1))
 
 adjective2 = input("Type in an Adjecttive")
 noun2 = input("Type in a noun")
 
-print("Then I did %s and %s" % (adjective2, noun2 ))
+print("Then, I walked over to the window and reflected on my sunny surroundings, a %s and %s" % (adjective2, noun2 ))
 
 adjective3 = input("Type in an Adjecttive")
 noun3 = input("Type in a noun")
 
-print("Now that I'm done, I go to %s and do %s" % (adjective3, noun3))
+print("Then I saw something in the distance, a %s and do %s" % (adjective3, noun3))
 
 adjective4 = input("Type in an Adjecttive")
 noun4 = input("Type in a noun")
 
-print("I su a %s and a %s" % (adjective4, noun4))
+print("It was a %s and a %s" % (adjective4, noun4))
 
+adjective5 = input("Type in an Adjecttive")
+noun5 = input("Type in a noun")
+
+print("As I stepped outside and as %s came closer, I could see the short smile on its face and %s.") % (adjective5, noun5)
+
+adjective6 = input("Type in an Adjecttive")
+noun6 = input("Type in a noun")
+
+print("%s gazed with the affection of %s understanding rapid rats. ") % (adjective6, noun6)
+
+adjective7 = input("Type in an Adjecttive")
+noun7 = input("Type in a noun")
+
+print("The %s and I looked at each other with jumpy feelings, like two kindly, knobbly koalas sleeping at a very articulate rave, which had %s music playing in the background") % (adjective7, noun7)
+
+adjective8 = input("Type in an Adjecttive")
+noun8 = input("Type in a noun")
+
+print("I regarded %s's wobbly fingers and saw it's %s") % (adjective8, noun8)
+
+adjective9 = input("Type in an Adjecttive")
+noun9 = input("Type in a noun")
+
+print("%s looked angry, and it's emotions were like a %s") % (adjective9, noun9)
+
+adjective10 = input("Type in an Adjecttive")
+noun10 = input("Type in a noun")
+
+print("Then, %s came inside for a nice cup of tea and some %s.") % (adjective10, noun10)
+
+print("THE END")
