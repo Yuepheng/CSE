371559cff8f7e014cbf3e48c3ce2d18bcbb9de1c
Yuepheng <42391752+Yuepheng@users.noreply.github.com>
2018-11-14 14:41:57 -0800

    11/14/2018

--- notes/venv/Lucky 7s.py
+++ notes/venv/Lucky 7s.py
@@ -1,10 +1,21 @@
 print("You have $15. Each time you play (round), you make a bet of $1. Two dice (D6) are rolled.")
 
 import random
-die_1 = random.randint(1, 6)
-die_2 = random.randint(1, 6)
+
 money = 15
 rounds = 0
 
-while money > 0:
-    if die_1 + die_2 == 7:
+while money >= 0:
+    die_1 = random.randint(1, 6)
+    die_2 = random.randint(1, 6)
+    roll = die_1 + die_2
+    rounds += 1
+    if roll == 7:
+        print("You gain some money")
+        print("you have %s money" % money)
+        print("You've passed %s rounds" % rounds)
+        money += 4
+    else:
+        money -= 1
+        print("you have %s money" % money)
+        print("You've passed %s rounds" % rounds)
