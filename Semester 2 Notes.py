11bc1aa12e0fa9d5e44156e40dbe19295d048a19
Yuepheng <42391752+Yuepheng@users.noreply.github.com>
2019-01-08 14:40:41 -0800

    1/8.2019

---
+++ Semester 2 Notes.py
@@ -0,0 +1,14 @@
+print("Hello World! :)")
+# This is a test
+
+cars = 5
+driving = True
+
+print("I have %s cars" % cars)
+
+age = input("How old are you?")
+print("%s?? Really" % age)
+
+colors = ["Blue", "Green", "Red", "Pink", "Maroon"]
+colors.append("Red")
+print(colors)
