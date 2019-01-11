ae3def157b98161b4a6c21feb16f770675763d1d
Yuepheng <42391752+Yuepheng@users.noreply.github.com>
2019-01-11 14:50:42 -0800

    1/11/2019

--- Yuepheng Xiong- Hangman.py
+++ Yuepheng Xiong- Hangman.py
@@ -1,3 +1,7 @@
+import random
 WordBank = ["Apple", "School", "Word", "Cat", "Run", "Notes",
             "Mad", "Can", "Fix", "Man", ]
+
+random.choice(0, 9)
+input("You have 8 guess to get the word right")
 
