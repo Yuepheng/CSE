7b9e8aa73545c8dd0eb7b6e5b987e19fce871094
Yuepheng <42391752+Yuepheng@users.noreply.github.com>
2019-01-31 14:38:28 -0800

    1/31/2019

--- Dictionary World Map By Yuepheng Xiong.py
+++ Dictionary World Map By Yuepheng Xiong.py
@@ -1,3 +1,4 @@
+
 world_map = {
     'R19A': {
         'NAME': "Mr. Wiebe's Room",
@@ -16,3 +17,24 @@ world_map = {
         }
     }
 }
+
+# Other Variables
+directions = ["NORTH", "SOUTH", "EAST", "WEST", "UP"]
+current_node = world_map["R19A"]  # THis is your current location
+playing = True
+
+# Controller
+while playing:
+    print(current_node['NAME'])
+
+    command = input(">_")
+    if command.lower() in ['q', 'quit', 'exit']:
+        playing = False
+    elif command in directions:
+        try:
+            room_name = current_node["PATHS"][command]
+            current_node = world_map[room_name]
+        except KeyError:
+            print("I can't Go that Way")
+    else:
+        print("Command Not recognized.")
