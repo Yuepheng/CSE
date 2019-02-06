89dc8a6518ea01d38827d984ca6da296b1401d65
Yuepheng <42391752+Yuepheng@users.noreply.github.com>
2019-02-06 14:36:58 -0800

    2/6/2019

--- Dictionary World Map By Yuepheng Xiong.py
+++ Dictionary World Map By Yuepheng Xiong.py
@@ -3,7 +3,8 @@ world_map = {
         'NAME': "Mr. Wade's Room",
         'DESCRIPTION': "THis is the room you are in",
         'PATHS': {
-            'NORTH': "PARKING_LOT"
+            'NORTH': "PARKING_LOT",
+            'WEST': "AMPHITHEATER"
 
         }
 
@@ -12,14 +13,16 @@ world_map = {
         'NAME': "A Parking Lot",
         'DESCRIPTION': "THere are a few cars parked here",
         'PATHS': {
-            'SOUTH': 'R19A'
+            'WEST': 'R19A',
+            'NORTH': 'DOWNTOWN DRESNO'
         }
     },
     'AMPHITHEATER': {
         'NAME': "The Stage outside",
         'DESCRIPTION': "There might be some items here",
         'PATHS': {
-            'SOUTH': 'R19A'
+            'NORTH': 'R19A',
+            'SOUTH': 'CAFETERIA'
         }
     },
     'CAFETERIA': {
@@ -27,14 +30,51 @@ world_map = {
         'DESCRIPTION': "There is some leftover non eaten ham and cheese bars and there might"
                        "be some items here",
         'PATHS': {
-            'SOUTH': 'AMPHITHEATER'
+            'SOUTH': 'AMPHITHEATER',
+            'NORTH': 'SB20'
         }
-    }
+    },
+    'SB20': {
+        'NAME': "Mr. Yang's Room",
+        'DESCRIPTION': "This guy loves Anime",
+        'PATHS': {
+            'EAST': 'CAFETERIA',
+            'SOUTH': 'PARKING_LOT',
+        }
+    },
+    'DOWNTOWN DRESNO': {
+        'NAME': "The City of Dresno",
+        'DESCRIPTION': "There are a lot of work offices here",
+        'PATHS': {
+            'SOUTH': 'ETHAN JAMES HIGH SCHOOL',
+            'EAST': 'GRANDPARENTS HOUSE',
+            'SOUTHEAST': 'PARENTS HOUSE',
+        }
+    },
+    'ETHAN JAMES HIGH SCHOOL': {
+        'NAME': "ETHAN JAMES HIGH SCHOOL GRADE 9-12",
+        'DESCRIPTION': "The School",
+        'PATHS': {
+            'NORTH': "DOWNTOWN DRESNO",
+            'WEST': "OFFICE"
+        }
+    },
+    'GRANDPARENTS HOUSE': {
+        'NAME': "My grandparents live here along with some aunties and uncles",
+        'DESCRIPTION': "There could be some items here",
+        'PATHS': {
+            'SOUTHWEST': "DOWNTOWN DRESNO",
+            'EAST': "PARENTS HOUSE",
 
-
-
-
-
+        }
+    },
+    'PARENTS HOUSE': {
+        'NAME': "My Mom, Dad, 2 sisters and a brother live here",
+        'DESCRIPTION': "There could be some helpful items here",
+        'PATHS': {
+            'SOUTHWEST': "GRANDPARENTS HOUSE"
+        }
+    },
 }
 
 # Other Variables
