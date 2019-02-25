66a5100213309c199afcdba0dc207f6a36446ed3
Yuepheng <42391752+Yuepheng@users.noreply.github.com>
2019-02-25 14:58:18 -0800

    2/25/2019

--- World Map OOP.py
+++ World Map OOP.py
@@ -17,17 +17,22 @@ parking_lot = Room("The Parking Lot", "THere are a few cars parked here", None, 
 Amphitheater = Room("The stage Outside", "There might be some items here", None, R19A)
 Cafeteria = Room("The Place to get food and chill", "There is some leftover non eaten baked ham and cheese bars and"
                                                     "there might be some items here", None, Amphitheater,)
-SB20 = Room("Mr Yang's Room", None, Cafeteria, parking_lot)
-Downtown_Dresno = Room("The City of Dresno", None, parking_lot)
-Ethan_James_High_School = Room("Ethan James High School Grades 9-12", None, Downtown_Dresno)
-Grandparents_House = Room("The house that my grandparents live in.", None, Ethan_James_High_School, Downtown_Dresno)
-Parents_House = Room("My mom, dad, 2 sisters and 1 brother live here", None, Grandparents_House)
-Office = Room("The Front desk of Ethan James High school", None, R19A)
-San_Framsico = Room("The City of Rich People AKA San Framsico", None, Downtown_Dresno)
-Cousins_House = Room("My Cousins live here", None, Parents_House)
-R13B = Room("Mr. Dawson's Room", None, SB20, R19A)
-SL15 = Room("Mrs. Hazen's Biology Room", None, R13B, SB20, R19A,)
-J_Mart = Room("The J mart to get food, technology, clothing etc.", None, Downtown_Dresno, Parents_House)
+SB20 = Room("Mr Yang's Room", "This guy loves Anime", None, Cafeteria, parking_lot)
+Downtown_Dresno = Room("The City of Dresno", "There are a lot of work offices here", None, parking_lot)
+Ethan_James_High_School = Room("Ethan James High School Grades 9-12", "The School", None, Downtown_Dresno)
+Grandparents_House = Room("The house that my grandparents live in.", "There could be some items here", None,
+                          Ethan_James_High_School, Downtown_Dresno)
+Parents_House = Room("My mom, dad, 2 sisters and 1 brother live here", "There could be some helpful items here", None,
+                     Grandparents_House)
+Office = Room("The Front desk of Ethan James High school", "You could talk to the VPs, assistants, and staff", None,
+              R19A)
+San_Framsico = Room("The City of Rich People AKA San Framsico", "I might need some items in the stores here",  None,
+                    Downtown_Dresno)
+Cousins_House = Room("My Cousins live here", "They might be of help to me", None, Parents_House)
+R13B = Room("Mr. Dawson's Room", "There could be some useful items here", None, SB20, R19A)
+SL15 = Room("Mrs. Hazen's Biology Room", "There might be some useful items here", None, R13B, SB20, R19A,)
+J_Mart = Room("The J mart to get food, technology, clothing etc.", "I might need some of the useful items here", None,
+              Downtown_Dresno, Parents_House)
 
 
 Parents_House.south = J_Mart
