d05e190db2ed5b801cbb2a2755fd6b08a3c40b7e
Yuepheng <42391752+Yuepheng@users.noreply.github.com>
2019-03-05 14:38:09 -0800

    3/5/2019

---
+++ Yuepheng Xiong Items.py
@@ -0,0 +1,34 @@
+class ModeOfTransportation(object):
+    def __init__(self, name):
+        self.name = name
+
+
+class Sedan(ModeOfTransportation):
+    def __init__(self, name):
+        super(Sedan, self).__init__(name)
+        self.Oil_Level = 100
+        self.battery_voltage = 15.7
+        self.fuel_level = 100
+        self.engine_on = False
+
+    def amount_of_oil(self):
+        self.Oil_Level -= .001
+        print("The Oil gauge reads that I still have LOTS of oil")
+
+    def the_battery_suppling_to_starter(self):
+        self.battery_voltage -= .005
+        print("The voltage meter reads that it is between 12 and 16 volts")
+
+    def amount_of_fuel(self):
+        self.fuel_level -= .0001
+        print("The Fuel gauge needle is on F for Full")
+
+    def starting_the_engine(self):
+        self.engine_on = True
+        print("I started the engine by stepping on the clutch and turning the key")
+
+    def move(self):
+        self.Oil_Level -= .001
+        self.fuel_level -= .0001
+        print("Good thing this Sedan has Power steering")
+
