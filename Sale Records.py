8f4013054462e4f0475b8bc0f9128d22f761ccd3
Yuepheng <42391752+Yuepheng@users.noreply.github.com>
2019-05-15 14:39:45 -0700

    5/15/2019

--- Sale Records.py
+++ Sale Records.py
@@ -19,6 +19,7 @@ with open("Sales Records.csv", 'r') as old_csv:
     AustraliaOceania_Profit = []
     Europe_Profit = []
     Asia_Profit = []
+    CentralAmericaAndCaribbean_Profit = []
     print("Processing....")
 
     for row in reader:
@@ -29,54 +30,75 @@ with open("Sales Records.csv", 'r') as old_csv:
             Fruit_Profit.append(float(Profit))
         HOUSEHOLD = row[2]
         if HOUSEHOLD == "Household":
+            print(Profit)
             Household_Profit.append(float(Profit))
         OFFICE_SUPPLIES = row[2]
         if OFFICE_SUPPLIES == "Office Supplies":
+            print(Profit)
             Office_Supplies_Profit.append(float(Profit))
         CLOTHES = row[2]
         if CLOTHES == "Clothes":
+            print(Profit)
             Clothes_Profit.append(float(Profit))
         MEAT = row[2]
         if MEAT == "Meat":
+            print(Profit)
             Meat_Profit.append(float(Profit))
         BEVERAGES = row[2]
         if BEVERAGES == "Beverages":
+            print(Profit)
             Beverages_Profit.append(float(Profit))
         COSMETICS = row[2]
         if COSMETICS == "Cosmetics":
+            print(Profit)
             Cosmetics_Profit.append(float(Profit))
         PERSONAL_CARE = row[2]
         if PERSONAL_CARE == "Personal Care":
+            print(Profit)
             Personal_Care_Profit.append(float(Profit))
         BABY_FOOD = row[2]
         if BABY_FOOD == "Baby Food":
+            print(Profit)
             Baby_Food_Profit.append(float(Profit))
         CEREAL = row[2]
         if CEREAL == "Cereal":
+            print(Profit)
             Cereal_Profit.append(float(Profit))
         VEGGIES = row[2]
         if VEGGIES == "Vegetables":
+            print(Profit)
             Veggies_Profit.append(float(Profit))
         SNACKS = row[2]
         if SNACKS == "Snacks":
+            print(Profit)
             Snacks_Profit.append(float(Profit))
         SUBSAHARAN = row[0]
         if SUBSAHARAN == "Sub-Saharan Africa":
+            print(Profit)
             SubSaharan_Africa_Profit.append(float(Profit))
         MIDDLEEASTANDNORTHAFRICA = row[0]
-        if MIDDLEEASTANDNORTHAFRICA == "Middle East and North Africa"
+        if MIDDLEEASTANDNORTHAFRICA == "Middle East and North Africa":
+            print(Profit)
             MiddleEastandNorthAfrica_Profit.append(float(Profit))
         AUSTRALIAOCEANIA = row[0]
-        if AUSTRALIAOCEANIA == "Australia and Ocreania":
+        if AUSTRALIAOCEANIA == "Australia and Oceania":
+            print(Profit)
             AustraliaOceania_Profit.append(float(Profit))
         EUROPE = row[0]
         if EUROPE == "Europe":
+            print(Profit)
             Europe_Profit.append(float(Profit))
         ASIA = row[0]
         if ASIA == "Asia":
+            print(Profit)
             Asia_Profit.append(float(Profit))
+        CENTRALAMERICAANDCARIBBEAN = row[0]
+        if CENTRALAMERICAANDCARIBBEAN == "Central America and the Caribbean":
+            print(Profit)
+            CentralAmericaAndCaribbean_Profit.append(float(Profit))
 
-
+    Asia_sum = sum(Asia_Profit)
+    CentralAmericaAndCaribbean_Sum = sum(CentralAmericaAndCaribbean_Profit)
     Europe_sum = sum(Europe_Profit)
     AustraliaOceania_sum = sum(AustraliaOceania_Profit)
     MiddleEastandNorthAfrica_sum = sum(MiddleEastandNorthAfrica_Profit)
@@ -111,9 +133,8 @@ with open("Sales Records.csv", 'r') as old_csv:
     print("New List about the profit from Regions.")
     print("The total Profit of the Sub Saharan Africa Region is % f" % SubSaharan_Africa_sum)
     print("The total profit of the Middle East and North Africa region is %f" % MiddleEastandNorthAfrica_sum)
-    print("The total Profit of the Australia And North Africa region is %f" % AustraliaOceania_sum)
+    print("The total Profit of the Australia And Oceania region is %f" % AustraliaOceania_sum)
     print("The total amount of Profit the Europe region is %f" % Europe_sum)
-    print("")
-
-
+    print("The total amount of Profit the Asia region has is %f" % Asia_sum)
+    print("The total amount of Profit the Central America and Caribbean region is %f" % CentralAmericaAndCaribbean_Sum)
     print("Done")
